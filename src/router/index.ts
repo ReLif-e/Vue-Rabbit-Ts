f52f03ada7f471c9427c638a4c640f19f299a538
baito <2606936464@qq.com>
2022-07-05 15:22:02 +0800

    样式初始化

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+// 创建路由1
+import {createRouter, createWebHistory} from 'vue-router'
+const router = createRouter({
+  routes:[
+    {}
+  ],
+  history:createWebHistory()
+})
+
+// 导出路由对象
+export default router
